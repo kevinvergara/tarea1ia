a19c31f7a8e56d424201a8cfb265499438b2ba4c
kevin vergara muñoz <kevin.vergara.lda@gmail.com>
2015-10-01 02:38:37 -0300

    esperanza

--- src/paquete/BFS.java
+++ src/paquete/BFS.java
@@ -1,5 +1,6 @@
 package paquete;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ public class BFS {
     public BFS() {
     }
     
-    public String [][] bfs(Nodo raiz){
+    public String [][] bfs(Nodo raiz) throws IOException{
         Matriz matrizClase = new Matriz();
         Operadores operadores = new Operadores();
         Scanner sc = new Scanner(System.in);
@@ -24,51 +25,64 @@ public class BFS {
             cola.encolar(raiz);            
             while(!cola.vacia()){
                 nodoActual = (Nodo)cola.frente();
+                System.out.println("\n");
+                matrizClase.imprimirMatriz(nodoActual.getMatriz());
+                System.out.println("\n");
                 cola.desencolar();
                 
                 int [] vectorAux = null;
                 vectorAux = operadores.actualizarPosicion(nodoActual.getMatriz(),(nodoActual.getNivel()+1));
                 if(vectorAux != null){
-                    String [][] matrizAux= new String[nodoActual.getMatriz().length][nodoActual.getMatriz().length];
-                    Nodo nodoAux= new Nodo();
-                    i=0;
-                    matrizAux = nodoActual.getMatriz();
-
-                    nodoAux.setColores(nodoActual.getColores());
-                    nodoAux.setNivel(nodoActual.getNivel()+1);
-                    nodoAux.setPosicion(vectorAux);
-
-
+                    String [][] matrizAux1= new String[nodoActual.getMatriz().length][nodoActual.getMatriz().length];
+                    String [][] matrizAux2= new String[nodoActual.getMatriz().length][nodoActual.getMatriz().length];
+                    String [][] matrizAux3= new String[nodoActual.getMatriz().length][nodoActual.getMatriz().length];
                     
-                    matrizAux[vectorAux[0]][vectorAux[1]]=nodoActual.getColores()[0];
-                    nodoAux.setMatriz(matrizAux);
+                    Nodo nodoAux1= new Nodo();
+                    Nodo nodoAux2=new Nodo();
+                    Nodo nodoAux3 = new Nodo();
+                   
+                    Matriz matrizInicial = new Matriz();
+                    
+                    matrizAux1 = matrizInicial.cargarMatriz();
+                    matrizAux2 = matrizInicial.cargarMatriz();
+                    matrizAux3 = matrizInicial.cargarMatriz();
+
+                    nodoAux1.setColores(nodoActual.getColores());
+                    nodoAux1.setNivel(nodoActual.getNivel()+1);
+                    nodoAux1.setPosicion(vectorAux);
+
+                    nodoAux2.setColores(nodoActual.getColores());
+                    nodoAux2.setNivel(nodoActual.getNivel()+1);
+                    nodoAux2.setPosicion(vectorAux);
+                    
+                    nodoAux3.setColores(nodoActual.getColores());
+                    nodoAux3.setNivel(nodoActual.getNivel()+1);
+                    nodoAux3.setPosicion(vectorAux);
+                    
+                    matrizAux1[vectorAux[0]][vectorAux[1]]=nodoActual.getColores()[0];
+                    nodoAux1.setMatriz(matrizAux1);
                     //System.out.println(nodoAux.getMatriz()[vectorAux[0]][vectorAux[1]]);
                     //auxi.setNodo((Nodo)nodoAux);
-                    cola.encolar(nodoAux);
+                  
                     
-                    
-                    matrizAux[vectorAux[0]][vectorAux[1]]=nodoActual.getColores()[1];
-                    nodoAux.setMatriz(matrizAux);
+                    matrizAux2[vectorAux[0]][vectorAux[1]]=nodoActual.getColores()[1];
+                    nodoAux2.setMatriz(matrizAux3);
                     //System.out.println(nodoAux.getMatriz()[vectorAux[0]][vectorAux[1]]);
                     //auxi.setNodo((Nodo)nodoAux);
-                    cola.encolar(nodoAux);
-                            
-                    matrizAux[vectorAux[0]][vectorAux[1]]=nodoActual.getColores()[2];
+                          
+                    matrizAux3[vectorAux[0]][vectorAux[1]]=nodoActual.getColores()[2];
                     
-                    nodoAux.setMatriz(matrizAux);
+                    nodoAux3.setMatriz(matrizAux3);
                     //System.out.println(nodoAux.getMatriz()[vectorAux[0]][vectorAux[1]]);
                     //auxi.setNodo((Nodo)nodoAux);
-                    cola.encolar(nodoAux);
+                   
                     //cola.encolar(auxi.getNodo(1));
                     //cola.encolar(auxi.getNodo(2));
+                    cola.encolar(nodoAux1); 
+                    cola.encolar(nodoAux2);
+                    cola.encolar(nodoAux3);
                     
-                    System.out.println(cola.tam());
-                    for(i=0;i<cola.size();i++){
-                        //matrizClase.imprimirMatriz(nodoActual.getNodo(i).getMatriz());
-                        //matrizClase.imprimirMatriz(((Nodo)cola.get(i)).getMatriz());
-                        System.out.println(((Nodo)cola.get(i)).getMatriz()[vectorAux[0]][vectorAux[1]]);
-                    }
-                    sc.nextLine();
+                       
 
                 }//listo
             }//fin while
